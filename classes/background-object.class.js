bd897bea1c543e348b401ddedb2d4f2cd1fb19a1
Benjamin Blarr <b.blarr@gmx.de>
2025-12-12 21:12:41 +0100

    Added background image and background objects

--- classes/background-object.class.js
+++ classes/background-object.class.js
@@ -1,11 +1,11 @@
 class BackgroundObject extends MoveableObject{
     
     width = 1440;
-    height = 800;
+    height = 960;
 
     constructor(imagePath, x, y){
         super().loadImage(imagePath);
-        this.y = y;
+        this.y = 960 - this.height;
         this.x = x;
     }
 
--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -5,7 +5,7 @@ class Chicken extends MoveableObject{
     constructor(){
         super().loadImage('../assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.x = 480 + Math.random() * 800;
-        this.y = 730;
+        this.y = 700;
         this.height = 150;
         this.width = 90;
     }

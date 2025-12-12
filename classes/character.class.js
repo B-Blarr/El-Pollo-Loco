f06a8d34fda60464243216d74753d50bc09c9ad7
Benjamin Blarr <b.blarr@gmx.de>
2025-12-12 22:09:50 +0100

    Added an imageHub for all images

--- classes/character.class.js
+++ classes/character.class.js
@@ -5,7 +5,7 @@ class Character extends MoveableObject{
 
         // wird immer dann als Erstes automatisch ausgeführt wenn irgendwo ein neues Objekt mit new Character() erstellt wird.
         constructor(){
-          super().loadImage('../assets/img/2_character_pepe/1_idle/idle/I-1.png');
+          super().loadImage(ImageHub.character.idle[0]);
 
         }
 

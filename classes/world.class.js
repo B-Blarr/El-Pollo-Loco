8f2e07dc691766732b18168053ec4c0e2ba3bfbb
Benjamin Blarr <b.blarr@gmx.de>
2025-12-13 20:45:04 +0100

    Added background images

--- classes/world.class.js
+++ classes/world.class.js
@@ -7,10 +7,24 @@ class World {
     new BackgroundObject("./assets/img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObject("./assets/img/5_background/layers/2_second_layer/1.png", 0),
     new BackgroundObject("./assets/img/5_background/layers/1_first_layer/1.png", 0),
+    new BackgroundObject("./assets/img/5_background/layers/air.png", 1440),
+    new BackgroundObject("./assets/img/5_background/layers/3_third_layer/2.png", 1440),
+    new BackgroundObject("./assets/img/5_background/layers/2_second_layer/2.png", 1440),
+    new BackgroundObject("./assets/img/5_background/layers/1_first_layer/2.png", 1440),
+    
+    new BackgroundObject("./assets/img/5_background/layers/air.png", 2880),
+    new BackgroundObject("./assets/img/5_background/layers/3_third_layer/1.png", 2880),
+    new BackgroundObject("./assets/img/5_background/layers/2_second_layer/1.png", 2880),
+    new BackgroundObject("./assets/img/5_background/layers/1_first_layer/1.png", 2880),
+    new BackgroundObject("./assets/img/5_background/layers/air.png", 4320),
+    new BackgroundObject("./assets/img/5_background/layers/3_third_layer/2.png", 4320),
+    new BackgroundObject("./assets/img/5_background/layers/2_second_layer/2.png", 4320),
+    new BackgroundObject("./assets/img/5_background/layers/1_first_layer/2.png", 4320),
   ];
   canvas;
   ctx;
   keyboard;
+  camera_x = 0;
 
   constructor(canvas, keyboard) {
     this.ctx = canvas.getContext("2d");
@@ -20,18 +34,21 @@ class World {
     this.setWorld();
   }
 
-  setWorld(){
+  setWorld() {
     this.character.world = this;
   }
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.ctx.translate(this.camera_x, 0);
+
     this.addObjectsToMap(this.backgroundObjects);
     this.addToMap(this.character);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemies);
 
+    this.ctx.translate(-this.camera_x, 0);
     // draw() wird immer wieder raufgerufen
     let self = this;
     requestAnimationFrame(function () {
@@ -40,7 +57,7 @@ class World {
   }
 
   addToMap(moveableObject) {
-    if(moveableObject.otherDirection){
+    if (moveableObject.otherDirection) {
       this.ctx.save();
       this.ctx.translate(moveableObject.width, 0);
       this.ctx.scale(-1, 1);

26d393cc79b2af92ef10093c3781f1b62ec16440
Benjamin Blarr <b.blarr@gmx.de>
2026-01-05 10:48:10 +0100

    Small change in audioerror class

--- classes/AudioError.class.js
+++ classes/AudioError.class.js
@@ -1,13 +1,11 @@
 class AudioError {
     static LONG = new Audio('');
 
-    // Spielt eine einzelne Audiodatei ab, - wenn sie bereit ist - 
     static playOne(sound) {
         setInterval(() => {  // Wiederholt die Überprüfung alle 200ms
-            //hier sound wird gestoppt
             if (sound.readyState == 4) {  // Überprüft, ob die Audiodatei vollständig geladen ist, wenn man die if abfrage rausnehmen würde, würde es bei start & drücken auf den stopp Knopf einen Fehler werfen. (am besten low-tier throttling nutzen!)
                 console.log("Sound ready"); 
-                sound.volume = 0.5;  // Setzt die Lautstärke auf 50%
+                sound.volume = 0.5; 
                 sound.play();  // Spielt das übergebene Sound-Objekt ab
             } else {
                 console.log("Sound not ready"); 
